ff2de037721e878ba36392bf9715b1cd406aa73c
Tyler Palesano <supersnapple@gmail.com>
2016-02-08 04:42:01 -0600

    Added Socket.IO 101
    Socket.IO 101 gives a few simple examples of how to send and receive
    messages.
    
    Also improved the UX.

--- index.js
+++ index.js
@@ -7,18 +7,26 @@ app.set('port', (process.env.PORT || 3000));
 
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/public/chat.html');
+app.get('/', function (request, response) {
+    response.sendFile(__dirname + '/public/chat.html');
 })
 
-app.get('/101', function(request, response) {
-  response.sendFile(__dirname + '/public/101.html');
+app.get('/101', function (request, response) {
+    response.sendFile(__dirname + '/public/101.html');
 })
 
 //Create "101" namespace
 var io_101 = io.of('/101');
-io_101.on('connection', function(socket){
-  console.log("holy guacamole this worked i'm so smart eeeeeee ");
+io_101.on('connection', function (socket) {
+    console.log("Socket id: " + socket.id + " connected.");
+
+    socket.on('a message from the client to the server', function (parameter) {
+        console.log("The server received the following message from " + socket.id + ": " + parameter);
+    })
+
+    socket.emit('a message from the server to the client', 'The socket says, "Hello"');
+    socket.broadcast.emit('a message from the server to the client', 'The socket says, "Hello" to all other sockets');
+    io_101.emit('a message from the server to the client', 'The server says, "Hello" to all sockets')
 });
 
 //Create "chat" namespace
@@ -26,72 +34,66 @@ var io_chat = io.of('/chat');
 // usernames which are currently connected to the "chat"
 var usernames = {};
 var numUsers = 0;
-io_chat.on('connection', function(socket){
-  console.log('a user connected');
-  var addedUser = false;
+io_chat.on('connection', function (socket) {
+    console.log('a user connected');
+    var addedUser = false;
 
-   socket.on('add user', function (uniqueIdentifer, username) {
-    // we store the username in the socket session for this client
-    socket.uniqueIdentifer = uniqueIdentifer;
-    socket.username = username;
-    // add the client's username to the global list
-    usernames[uniqueIdentifer] = username;
-    ++numUsers;
-    addedUser = true;
-    socket.emit('login', {
-      numUsers: numUsers
+    socket.on('add user', function (uniqueIdentifer, username) {
+        // we store the username in the socket session for this client
+        socket.uniqueIdentifer = uniqueIdentifer;
+        socket.username = username;
+        // add the client's username to the global list
+        usernames[uniqueIdentifer] = username;
+        ++numUsers;
+        addedUser = true;
+        socket.emit('login', {
+            numUsers: numUsers
+        });
+        // echo globally (all clients) that a person has connected
+        socket.broadcast.emit('user joined', {
+            username: socket.username,
+            numUsers: numUsers
+        });
     });
-    // echo globally (all clients) that a person has connected
-    socket.broadcast.emit('user joined', {
-      username: socket.username,
-      numUsers: numUsers
+
+    socket.on('disconnect', function () {
+        console.log('user disconnected: ' + socket.username);
+
+        // remove the username from global usernames list
+        if (addedUser) {
+            delete usernames[socket.uniqueIdentifer];
+            --numUsers;
+
+            // echo globally that this client has left
+            socket.broadcast.emit('user left', {
+                username: socket.username,
+                numUsers: numUsers
+            });
+        }
     });
-  });
 
-  socket.on('disconnect', function(){
-    console.log('user disconnected: ' + socket.username);
+    socket.on('chat message', function (username, msg) {
+        if (username != socket.username) {
+            //The user has decided to change their name.
+            usernames[socket.uniqueIdentifer] = username;
 
-	// remove the username from global usernames list
-    if (addedUser) {
-      delete usernames[socket.uniqueIdentifer];
-      --numUsers;
+            io_chat.emit('user renamed', {
+                oldUsername: socket.username,
+                username: username
+            });
 
-      // echo globally that this client has left
-      socket.broadcast.emit('user left', {
-        username: socket.username,
-        numUsers: numUsers
-      });
-  }});
+            socket.username = username;
+        }
 
-   socket.on('chat message', function(username, msg){
-    if(username != socket.username)
-    {
-      //The user has decided to change their name.
-      usernames[socket.uniqueIdentifer] = username;
-
-      io_chat.emit('user renamed', {
-        oldUsername: socket.username,
-        username: username
-      });
-
-      socket.username = username;
-    }
-
-    console.log(socket.username + ': ' + msg);
-  	io_chat.emit('chat message', {
-        username: socket.username,
-        uniqueIdentifer: socket.uniqueIdentifer,
-        message: msg
-      });
-
-	//Sends to everyone else...
-	//socket.broadcast.emit('chat message', {
-    //  username: socket.username,
-    //  message: msg
-    //});
-  });
+        console.log(socket.username + ': ' + msg);
+        io_chat.emit('chat message', {
+            username: socket.username,
+            uniqueIdentifer: socket.uniqueIdentifer,
+            message: msg
+        });
+    });
 });
 
-http.listen(app.get('port'), function(){
-  console.log("Node server is listening on port:" + app.get('port'))
+http.listen(app.get('port'), function () {
+    console.log("Node server is listening on port:" + app.get('port'))
 });
--- public/101.js
+++ public/101.js
@@ -1,3 +1,9 @@
-(function() {
-	var socket = io('/101');
+(function () {
+    var socket = io('/101');
+
+    socket.emit('a message from the client to the server', "The client says, 'Hello!'");
+
+    socket.on('a message from the server to the client', function (messageFromServer) {
+        document.body.innerHTML += "<div>" + messageFromServer + "</div>";
+    });
 })();
